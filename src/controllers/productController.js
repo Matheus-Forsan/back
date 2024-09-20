6fb6bf0a58d56c4f1f6fd5bd3af040412b09dd69
Matheus-Forsan <matheusforsan@gmail.com.com>
2024-09-20 08:58:11 -0300

    get

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,4 @@
-import { getAllProducts } from "../services/productService.js";
-
+import { getAllProducts, createProduct, getProductById } from "../services/productService.js";
 
 export const getProducts = async (req, res)=>{
     try { 
@@ -12,3 +11,31 @@ export const getProducts = async (req, res)=>{
         });
     }
 };
+
+export const createProductController = async (req, res) =>{
+    try {
+        const newProduct = await createProduct(req.body);
+        res.status(201).json(newProduct)
+    }
+    catch (error) {
+        res.status(500).json({error: "Erro ao criar produto"
+            
+        });
+    }
+};
+
+export const getProduct = async (req, res) =>{
+    try {
+        const product = await getProductById(req.params.id)
+        if(product){
+            res.json(product);
+        } else {
+            res.status(500).json({error: "Produto não encontrado"}
+
+            )};
+    }
+    catch(error){
+        res.status(500).json({error: "erro ao buscar produto"})
+    }
+};
+
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,11 @@
-import { getProducts } from "../controllers/productController.js"
+import { getProducts, createProductController, getProduct } from "../controllers/productController.js"
 import { Router } from "express"
 
 
 const router = Router();
 
 router.get("/products", getProducts);
+router.get("/products/:id", getProduct);
+router.post("/products", createProductController);
 
 export default router;
